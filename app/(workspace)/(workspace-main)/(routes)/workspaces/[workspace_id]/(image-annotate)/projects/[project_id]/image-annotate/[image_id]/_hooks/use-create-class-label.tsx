106ce8fde61e1f2b63b878fc65724cb3cde8584b
OH-JOO-YEONG <brb1111@naver.com>
2025-03-05 07:37:44 +0900

    [Feat] Class Label 생성 API 연동

---
+++ app/(workspace)/(workspace-main)/(routes)/workspaces/[workspace_id]/(image-annotate)/projects/[project_id]/image-annotate/[image_id]/_hooks/use-create-class-label.tsx
@@ -0,0 +1,37 @@
+import axiosInstance from '@/config/axios-instance';
+import { useMutation } from '@tanstack/react-query';
+import { LabelClassType } from '../_types/label-class';
+
+type CreateClassLabel = {
+  name: string;
+  description?: string;
+  type: LabelClassType;
+  color: string;
+  workspaceId: string;
+  projectId: string;
+};
+
+export default function useCreateClassLabel() {
+  const mutate = useMutation({
+    mutationFn: async ({
+      name,
+      description,
+      type,
+      color,
+      workspaceId,
+      projectId,
+    }: CreateClassLabel) => {
+      const { data } = await axiosInstance.post(
+        `/workspaces/${workspaceId}/projects/${projectId}/labels`,
+        {
+          name,
+          description,
+          type,
+          color,
+        },
+      );
+      return data;
+    },
+  });
+  return mutate;
+}
