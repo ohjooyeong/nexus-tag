39fc059ca3e927d8ff7ef230c2a261d5093653ce
OH-JOO-YEONG <brb1111@naver.com>
2025-03-20 03:42:30 +0900

    [Feat] 프로젝트 리스트 페이지네이션, 프로젝트 카드 이미지 추가

--- app/(workspace)/(workspace-main)/(routes)/workspaces/[workspace_id]/projects/_components/project-list.tsx
+++ app/(workspace)/(workspace-main)/(routes)/workspaces/[workspace_id]/projects/_components/project-list.tsx
@@ -6,30 +6,107 @@ import {
 } from '@/app/(workspace)/(workspace-main)/_types';
 import ProjectCard from './project-card';
 import NoResults from '../../../../../_components/no-results';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+import { cn } from '@/lib/utils';
 
 type Props = {
   isLoading: boolean;
   data: ProjectsResponse;
+  handlePageChange: (newPage: number) => void;
+  page: number;
+  totalPages: number;
 };
 
-const ProjectList = ({ isLoading, data }: Props) => {
+const ProjectList = ({
+  isLoading,
+  data,
+  handlePageChange,
+  page,
+  totalPages,
+}: Props) => {
   return (
-    <div className="flex flex-wrap gap-6">
-      {isLoading && (
-        <>
-          <ProjectCard.Skeleton />
-          <ProjectCard.Skeleton />
-          <ProjectCard.Skeleton />
-          <ProjectCard.Skeleton />
-          <ProjectCard.Skeleton />
-          <ProjectCard.Skeleton />
-        </>
-      )}
-      {data?.projects.map((project: Project) => (
-        <ProjectCard key={project.id} project={project} />
-      ))}
-      {data?.projects.length === 0 && <NoResults />}
-    </div>
+    <>
+      <div className="flex flex-wrap gap-6">
+        {isLoading && (
+          <>
+            <ProjectCard.Skeleton />
+            <ProjectCard.Skeleton />
+            <ProjectCard.Skeleton />
+            <ProjectCard.Skeleton />
+            <ProjectCard.Skeleton />
+            <ProjectCard.Skeleton />
+          </>
+        )}
+        {data?.projects.map((project: Project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+
+        {data?.projects.length === 0 && <NoResults />}
+      </div>
+      <div className="flex items-center justify-center py-4 min-h-8">
+        <Pagination className={cn(totalPages === 1 && 'hidden')}>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                onClick={() => page > 1 && handlePageChange(page - 1)}
+                className={
+                  page <= 1
+                    ? 'pointer-events-none opacity-50'
+                    : 'cursor-pointer'
+                }
+              />
+            </PaginationItem>
+
+            {[...Array(totalPages)].map((_, index) => {
+              const pageNumber = index + 1;
+
+              if (
+                pageNumber === 1 ||
+                pageNumber === totalPages ||
+                (pageNumber >= page - 1 && pageNumber <= page + 1)
+              ) {
+                return (
+                  <PaginationItem key={pageNumber}>
+                    <PaginationLink
+                      onClick={() => handlePageChange(pageNumber)}
+                      isActive={page === pageNumber}
+                    >
+                      {pageNumber}
+                    </PaginationLink>
+                  </PaginationItem>
+                );
+              } else if (pageNumber === page - 2 || pageNumber === page + 2) {
+                return (
+                  <PaginationItem key={pageNumber}>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                );
+              }
+              return null;
+            })}
+
+            <PaginationItem>
+              <PaginationNext
+                onClick={() => page < totalPages && handlePageChange(page + 1)}
+                className={
+                  page >= totalPages
+                    ? 'pointer-events-none opacity-50'
+                    : 'cursor-pointer'
+                }
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+    </>
   );
 };
 
