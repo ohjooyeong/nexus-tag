738a5307154432f62df649f2ce450024e452e00a
OH-JOO-YEONG <brb1111@naver.com>
2025-03-21 21:01:51 +0900

    [Fix] 클래스 라벨이 미 선택시 그리기 툴 선택 못하게 수정

--- app/(workspace)/(workspace-main)/(routes)/workspaces/[workspace_id]/(image-annotate)/projects/[project_id]/image-annotate/[image_id]/_store/tool-store.ts
+++ app/(workspace)/(workspace-main)/(routes)/workspaces/[workspace_id]/(image-annotate)/projects/[project_id]/image-annotate/[image_id]/_store/tool-store.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand';
 import { Tool } from '../_types/types';
 import { usePanningStore } from './panning-store';
+import { useClassLabelStore } from './class-label-store';
+import { toast } from 'sonner';
 
 interface State {
   toolId: Tool;
@@ -29,6 +31,19 @@ export const useToolStore = create<ToolState>((set, get) => ({
     } else {
       setEnabledPanning(false);
     }
+
+    const getActiveClassLabel =
+      useClassLabelStore.getState().getActiveClassLabel;
+
+    if (
+      toolId !== Tool.Pan &&
+      toolId !== Tool.Selection &&
+      getActiveClassLabel() === null
+    ) {
+      toast.error('Please create and select a class label first');
+      return;
+    }
+
     set({ toolId });
   },
 
